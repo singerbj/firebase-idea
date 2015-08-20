9b697b2ab8ad97a58f81044573b4af25accb5202
Ben Singer <benjamin.singer@helpsystems.com>
2015-08-20 15:48:53 -0500

    working now

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 var app = require('express')();
 var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-var io = require('socket.io')(server, {origins:'*'});
+var WebSocketServer = require('ws').Server;
 var JsonDB = require('node-json-db');
 var db = new JsonDB("myDB", true, false);
 
-server.listen(80);
+server.listen(1337);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  io.emit('this', { will: 'be received by everyone'});
-
-  socket.on('user', function (user) {
-    console.log("user = " + user);
-    io.emit('msg', "user = " + user);
-  });
-  socket.on('like', function (like) {
-    console.log("like = " + like);
-    io.emit('msg', "like = " + like);
+var wss = new WebSocketServer({ port: 8081 });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    wss.clients.forEach(function(ws){
+        ws.send('received: ' + message);
+    });
   });
 
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
+  ws.on('close', function(){
+    var i = wss.clients.indexOf(ws);
+    if(wss.clients.indexOf(ws) > -1){
+      delete wss.clients[i];
+    }
+  });
+
+  ws.on('error', function(err){
+    console.log(err);
   });
 });
