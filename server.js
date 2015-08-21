02748fa9dfd45133fdcc2d9103e22aea23dc6521
Ben Singer <benjamin.singer@helpsystems.com>
2015-08-21 15:04:42 -0500

    moar neet stuufff

--- server.js
+++ server.js
@@ -10,12 +10,32 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+var usersInc = 0;
+var likesInc = 0;
+var users = {};
+var likes = {};
+
 var wss = new WebSocketServer({ port: 8081 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log('received: ' + message);
+
+    var obj = JSON.parse(message);
+    if(obj.type === 'user'){
+        usersInc += 1;
+        obj.id = usersInc;
+        delete obj.type;
+        users[usersInc] = obj;
+    }else if(obj.type === 'like'){
+        likesInc += 1;
+        obj.id = likesInc;
+        delete obj.type;
+        likes[likesInc] = obj;
+    }
+
     wss.clients.forEach(function(ws){
-        ws.send('received: ' + message);
+        var data = { users: users, likes: likes };
+        ws.send(JSON.stringify(data));
     });
   });
 
